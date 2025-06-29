9134b104a8d98c89319afb7e8036d3bfb6c72570
AbuHumayd1 <daarussalaam22@gmail.com>
2025-06-29 23:09:59 +0100

    corrected_bks_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,12 +34,12 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
+C-- Create Order_Details table with quantity as DOUBLE
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
